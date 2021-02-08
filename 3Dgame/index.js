06037cef902323d0ee58392cc75749d751654b34
runoob f <3024446880@qq.com>
2021-02-09 00:28:11 +0800

    blog

---
+++ 3Dgame/index.js
@@ -0,0 +1,3 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>var createGame = require('voxel-engine');
+var game = createGame();
+game.appendTo(document.body);
